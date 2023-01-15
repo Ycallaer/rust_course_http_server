4b00388c1b194e732dcdf9d3979739e16f064dde
yves <yves_callaert@hotmail.com>
2023-01-15 12:52:38 +0100

    routing requests

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,12 +1,19 @@
 use crate::http::Request;
 
 use super::server::Handler;
-use super::http::{Response,StatusCode};
+use super::http::{Response,StatusCode, Method};
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+              "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+              "/hello" => Response::new(StatusCode::Ok, Some("<h1>hello</h1>".to_string())),
+              _ => Response::new(StatusCode::NotFound, None)
+            }
+            _ => Response::new(StatusCode::NotFound, None)
+        }
     }
 }
